1495e9956e300953b710f0a6c4e4781d148d08d7
nlifset1 <nlifset1@gmail.com>
2019-02-14 14:16:40 -0800

    Added PSF to the cube

--- src/median_calculation/cube.hpp
+++ src/median_calculation/cube.hpp
@@ -53,6 +53,8 @@ class cube {
     assert(m_size_k <= m_timestamp_list.size());
         m_exposuretime_list(std::move(exposuretime_list)) {
     assert(m_size_k <= m_exposuretime_list.size());
+        m_psf_list(std::move(psf_list)) {
+    assert(m_size_k <= m_psf_list.size());
   }
 
   ~cube() = default; // Default destructor
@@ -105,6 +107,11 @@ class cube {
     assert(k < m_exposuretime_list.size());
     return m_exposuretime_list[k];
   }
+  
+  double psf(const size_t k) const {
+    assert(k < m_psf_list.size());
+    return m_psf_list[k];
+  }
 
  private:
   /// -------------------------------------------------------------------------------- ///
@@ -155,6 +162,7 @@ class cube {
 
   std::vector<double> m_timestamp_list; // an array of the timestamp of each frame.
   std::vector<double> m_exposuretime_list; // an array of the exposure time of each frame.
+  std::vector<double> m_psf_list; // an array of the psf fwhm of each frame.
 };
 
 } // namespace median
