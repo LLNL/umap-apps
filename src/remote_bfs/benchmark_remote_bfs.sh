9bf315d3c6129e93c9c4d2f215545e63ee3b04d4
IP <56741918+IBPeng@users.noreply.github.com>
2020-05-06 09:19:11 -0700

    update example

--- src/remote_bfs/benchmark_remote_bfs.sh
+++ src/remote_bfs/benchmark_remote_bfs.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 EXE="./bin/remote_bfs"
-dir="/p/lscratchh/peng8"
 
-margo_lib_dir="/g/g90/peng8/flash/deps/lib"
-umap_lib_dir="/g/g90/peng8/flash/forked_umap_remote_region/build4/lib"
-lpath="LD_LIBRARY_PATH=${margo_lib_dir}:${umap_lib_dir}:$LD_LIBRARY_PATH"
+if test -z "$input_dir";then
+   input_dir="$(pwd)"
+fi
 
-K=1024
-M=$((K*K))
-G=$((K*K*K))
+if test -z "$margo_lib_dir";then
+    echo "$margo_lib_dir not set!"
+    exit
+fi
+
+lpath="LD_LIBRARY_PATH=${margo_lib_dir}:$LD_LIBRARY_PATH"
 
 prepare_input(){
     if test ! -f $graph_file_path ;then
 	numThreads=8
-	cmd="env $lpath OMP_NUM_THREADS=$numThreads ./bin/generate_edge_list -o $dir/edge_list -v $scale -e $((2 ** scale * 16))"
+	cmd="env $lpath OMP_NUM_THREADS=$numThreads ./bin/generate_edge_list -o ${input_dir}/edge_list -v $scale -e $((2 ** scale * 16))"
 	echo $cmd
 	eval $cmd
 
@@ -22,31 +24,18 @@ prepare_input(){
 	t=$(( numThreads-1 ))
 	for i in $(seq 0 $t)
 	do
-	    cmd=$cmd" "$dir/edge_list_$i
+	    cmd=$cmd" "${input_dir}/edge_list_$i
 	done
 	echo $cmd
 	eval $cmd
     fi
 }
 
-print_gcc_version() {
-    echo ""
-    ret=$(strings $1 | grep "GCC")
-    echo ${ret}
-    echo ""
-}
-
-
-# -------------------------------------------------------- #
-# Run benchmark varying configuration
-# -------------------------------------------------------- #
 main() {
-    # ---- Print some system information ---- #
-    print_gcc_version $EXE
 
-    for scale in 27 28 29;do
+    for scale in 27 28 29 30;do 
 	
-	graph_file_path=$dir"/csr_graph_s${scale}"
+	graph_file_path=${input_dir}"/csr_graph_s${scale}"
 	out_prefix="bfs_s${scale}"
     
 	num_vertices=$(( 2**scale ))
@@ -57,36 +46,38 @@ main() {
 	BASE_OPTIONS="-n${num_vertices} -m${num_edges} -g${graph_file_path}"
 
 	# ---- Start the server  ---- #
-	for numServerNode in 4 3 2 1;do
-	    for numServerProc in 1 2 4;do
+	for numServerNode in 2;do
+	    for numServerProc in 4;do
 		rm -rf serverfile
 		serverThreads=$(( 24/numServerProc ))
-		cmd="env $lpath OMP_NUM_THREADS=$serverThreads UMAP_PAGESIZE=4194304 srun --ntasks-per-node=${numServerProc} -N ${numServerNode} $EXE ${BASE_OPTIONS} -s &"
+		cmd="env $lpath OMP_NUM_THREADS=$serverThreads UMAP_PAGESIZE=1048576 srun --ntasks-per-node=${numServerProc} -N ${numServerNode} $EXE ${BASE_OPTIONS} -s &"
 		echo $cmd
 		eval $cmd
 
 		# -----Start the client ----- #
 		while [ ! -f serverfile ]; do
-		    sleep 3
+		    sleep 10
 		done
+		sleep 30
 
-		for numClientNodes in 4 3 2 1;do
-		    for numClientProcPerNode in 1 2 4;do
-			numOMPThreads=$(( 24/numClientProcPerNode ))
-			for pSize in $((1*M)) $((256*K)) #$((64*K)) $((16*K)) # $((4*K)) $((1*M)) $((4*M)) #umap_page_size
-			do
+		for numClientNodes in 4;do
+		    for numClientProcPerNode in 1 ;do
+			for k in 128 16;do
+			    numOMPThreads=$(( 24/numClientProcPerNode ))
+			    pSize=$(( k * 1024 ))
 			    cmd="env $lpath OMP_NUM_THREADS=$numOMPThreads OMP_SCHEDULE=static UMAP_PAGESIZE=$pSize srun --ntasks-per-node=$numClientProcPerNode -N $numClientNodes $EXE ${BASE_OPTIONS}"
 			    echo $cmd
 			    time eval $cmd
+			    sleep 120
 			done
 		    done
 		done
+		pkill remote_bfs
+		sleep 120
 	    done
 	done
     done
     
-    pkill remote_bfs
-    sleep 3
     echo "Done"
 }
 
